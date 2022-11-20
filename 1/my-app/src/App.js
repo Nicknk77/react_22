6be01d445f2c6074ca8f54f7a7393463e5093ca4
Nikolay Kutepov <nkutepov@hotmail.com>
2022-11-20 19:44:43 +0300

    home-task-3

--- 1/my-app/src/App.js
+++ 1/my-app/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import './components/message.css'
 import { Message } from './components/Message';
 import { useEffect, useState } from 'react';
+import { ListItem } from '@mui/material';
+import { List } from '@mui/material';
 
 function App() {
   const [mess, setMess] = useState('');
@@ -23,10 +25,12 @@ function App() {
     ev.preventDefault();
     setAuthorInput(document.querySelector('#authorId').value);
     sendToArray();
+    document.querySelector('#authorId').focus();
   }
   const sendToArray = () => {
     if (mess.length > 0 && document.querySelector('#authorId').value.length > 0)
-      setMessageList([...messageList, { id: messageList.length, author: document.querySelector('#authorId').value, text: mess }]);
+      setMessageList([...messageList, { author: document.querySelector('#authorId').value, text: mess }]);
+
   }
 
   useEffect(() => {
@@ -35,21 +39,33 @@ function App() {
 
   return (
     <div className="App">
-      <form onSubmit={sendData}>
-        <legend>Enter author and text here.</legend>
-        <input type="text" id='authorId' />
-        <input type="text" onChange={setMessage} />
-        <Message mess={mess} />
-        <button className='butSend'>Send massage</button>
-      </form>
-      <ul className='listMessage'>
-        {messageList.map(item => {
-          return <li key={item.id}><span>{item.author}</span>: {item.text}</li>
-        })}
-      </ul>
-      <h2>{greet}</h2>
+      <div>
+        <List>
+          {messageList.map((item, id) => {
+            ListItem.author = item.author;
+            ListItem.text = item.text;
+            return <ListItem key={id}><span>{ListItem.author}</span>: {ListItem.text}</ListItem>
+          })}
+        </List>
+      </div>
+      <div>
+        <form onSubmit={sendData}>
+          <legend>Enter author and text here.</legend>
+          <input type="text" id='authorId' autoFocus={true} />
+          <input type="text" onChange={setMessage} />
+          <Message mess={mess} />
+          <button className='butSend'>Send massage</button>
+        </form>
+        <ul className='listMessage'>
+          {messageList.map((item, id) => {
+            return <li key={id}><span>{item.author}</span>: {item.text}</li>
+          })}
+        </ul>
+        <h2>{greet}</h2>
 
+      </div>
     </div>
+
   );
 }
 
